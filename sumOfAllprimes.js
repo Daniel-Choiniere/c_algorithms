f43c73230cc6272e2e15c5e69581a42574214a13
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-05-28 22:07:03 +0000

    completed solution for summing all odd fibinacci numbers

--- sumOfAllprimes.js
+++ sumOfAllprimes.js
@@ -2,23 +2,29 @@
 // A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number
 // because it's only divisible by one and two.
 
-function isPrime(value) {
-    var counter = 0;
-    for(var i = 2; i < value; i++)
-    {
-        if(value % i === 0)
-        {
-            counter += i;
-            // console.log("Not a prime number");
-            // return value;
-        }
-        else if (value % i !== 0 || value === 2)
-        {
-            console.log("This is a prime number");
-            // return value;
-        }
-        console.log(counter);
+function isPrime(val){
+
+  //test if number is prime
+  for(var i=2; i < val; i++){
+    if(val % i === 0){
+      return false;
     }
+  }
+  return true;
 }
 
-isPrime(10);
+function sumPrimes(num) {
+  var answer = 0;
+
+  //loop through all numbers from 2 to input value
+  for(var i=2; i <= num; i++){
+
+    //sum only prime numbers, skip all others
+    if(isPrime(i)){
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+sumPrimes(977);
